d4a85a72b4a63f7f727df69fdd44ffba03139a38
Johnny German Cubides Castro <jgcubidesc@unal.edu.co>
2017-01-16 16:55:22 -0500

    runMain.py

--- server/runMain.py
+++ server/runMain.py
@@ -18,6 +18,9 @@ s.connect((ip, port))
 def main():
     s.send('\r')
     time.sleep(0.2) # Delay for 200mS
+    s.send('import pyb\r')
+    s.send('receivedLed = pyb.LED(1)\r')
+    s.send('receivedLed.on()\r')
     s.send('\x05')  # Start mode paste
     with open(nameFile) as fp: 
         for line in fp:
@@ -25,6 +28,7 @@ def main():
             time.sleep(timeDelay)
             print(line)
     s.send('\x04')  # Execute
+    s.send('receivedLed.off()\r')
 
 main()
 s.close()
