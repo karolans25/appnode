a168dae6347d5567f32150198286f65717bf1297
Johnny German Cubides Castro <jgcubidesc@unal.edu.co>
2017-01-16 13:01:53 -0500

    Soporte para websocket
    Se ha dado el soporte para la seguridad websocket
    hash sha1
    subprotocol
    Switch telnet o websocket

--- installMicropython/nodemcu/scripts/iotmqtt.lua
+++ installMicropython/nodemcu/scripts/iotmqtt.lua
@@ -3,6 +3,7 @@ gpio.mode(4, gpio.OUTPUT)
 stm = {}
 stm_out = false
 MQTT_Client = nil
+telnet = true
 
 function thingspeak(apikey, datas)
 gpio.write(4,gpio.LOW)
@@ -100,19 +101,120 @@ end
 
 sv=net.createServer(net.TCP, 180)
 global_c = nil
+
 sv:listen(3335, function(c)
 	if global_c~=nil then
 		global_c:close()
 	end
-	global_c=c 
-	c:on("receive",function(sck,pl)	uart.write(0,pl) end)
+local band = bit.band
+local bor = bit.bor
+local rshift = bit.rshift
+local lshift = bit.lshift
+local char = string.char
+local byte = string.byte
+local sub = string.sub
+local applyMask = crypto.mask
+
+local function decode(chunk)
+  if #chunk < 2 then return end
+  local second = byte(chunk, 2)
+  local len = band(second, 0x7f)
+  local offset
+  if len == 126 then
+    if #chunk < 4 then return end
+    len = bor(
+      lshift(byte(chunk, 3), 8),
+      byte(chunk, 4))
+    offset = 4
+  elseif len == 127 then
+    if #chunk < 10 then return end
+    len = bor(
+      -- Ignore lengths longer than 32bit
+      lshift(byte(chunk, 7), 24),
+      lshift(byte(chunk, 8), 16),
+      lshift(byte(chunk, 9), 8),
+      byte(chunk, 10))
+    offset = 10
+  else
+    offset = 2
+  end
+  local mask = band(second, 0x80) > 0
+  if mask then
+    offset = offset + 4
+  end
+  if #chunk < offset + len then return end
+
+  local first = byte(chunk, 1)
+  local payload = sub(chunk, offset + 1, offset + len)
+  assert(#payload == len, "Length mismatch")
+  if mask then
+    payload = applyMask(payload, sub(chunk, offset - 3, offset))
+  end
+  local extra = sub(chunk, offset + len + 1)
+  local opcode = band(first, 0xf)
+  return extra, payload, opcode
+end
+	global_c=c
+	c:on("receive", function(sck,pl) 
+		if string.match(pl,'^.-\r\n') == 'GET / HTTP/1.1\r\n' then
+			pl = string.gsub(string.match(pl,'Sec--WebSocket--Key.+=='),'Sec--WebSocket--Key: ',"")
+			pl = crypto.toBase64(crypto.hash("sha1",pl..'258EAFA5-E914-47DA-95CA-C5AB0DC85B11'))
+			global_c:send("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: " ..pl.. "\r\n\r\n")
+			telnet = false
+		else
+			if telnet == true then
+				uart.write(0,pl)
+			else
+				local extra, payload, opcode = decode(pl)
+				uart.write(0,payload)
+			end
+		end
+	end)
+	c:on("disconnection", function(c)
+		telnet = true
+    end)
 end)
 
 uart.on("data",'\r', function(data)
+
+local band = bit.band
+local bor = bit.bor
+local rshift = bit.rshift
+local lshift = bit.lshift
+local char = string.char
+local byte = string.byte
+
+local function encode(payload)
+  local opcode = 1 
+  assert(type(opcode) == "number", "opcode must be number")
+  assert(type(payload) == "string", "payload must be string")
+  local len = #payload
+  local head = char(
+    bor(0x80, opcode),
+    len < 126 and len or (len < 0x10000) and 126 or 127
+  )
+  if len >= 0x10000 then
+    head = head .. char(
+    0,0,0,0, -- 32 bit length is plenty, assume zero for rest
+    band(rshift(len, 24), 0xff),
+    band(rshift(len, 16), 0xff),
+    band(rshift(len, 8), 0xff),
+    band(len, 0xff)
+  )
+  elseif len >= 126 then
+    head = head .. char(band(rshift(len, 8), 0xff), band(len, 0xff))
+  end
+  return head .. payload
+end
+
 	if global_c~=nil then
 		if gpio.read(0) == 0 then
 			if stm_out == false then
-				global_c:send(data..'\n')
+				if telnet == true then
+					global_c:send(data..'\n')
+				else
+					global_c:send(encode(data))
+				end
 			else
 				stm_out = false
 				taskstm()
